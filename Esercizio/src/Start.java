0890a128232350ba7242299f1a72183af35672b6
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-22 22:20:55 +0100

    Esercizio 2 sulle stringhe

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -2,8 +2,8 @@ public class Start {
     public static void main(String[] args) {
         String string1 = "United Kingdom";
         String string2 = "Germany";
-        String sub1 = string1.substring(0,6);
-        String sub2 = string1.substring(7);
+        String sub1 = string1.substring(0,string1.indexOf(" "));
+        String sub2 = string1.substring(string1.indexOf(" ") +1);
         String charArray = sub1.toUpperCase() + " " + string2.toUpperCase() + " " + sub2.toUpperCase();
         System.out.println(charArray);
     }
